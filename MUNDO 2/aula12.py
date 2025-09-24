abc4d01af8829fc14d8e76c7cf3a3fef46a2c9fa
Nathielle <nathiellesilvasz@gmail.com>
2025-09-24 17:17:34 -0300

    Update aula12.py

--- MUNDO 2/aula12.py
+++ MUNDO 2/aula12.py
@@ -1,6 +1,8 @@
 nome = str(input('Qual é o seu nome?'))
 if nome == 'Gustavo':
     print('que nome bonito')
+elif nome == 'Maria' or nome == 'João' or nome == 'Lucas':
+     print ('Seu nome é bem normal no Brasil')
 else:
     print('Seu nome é bem normal')
 print('Tenha um bom dia {}'.format(nome))
